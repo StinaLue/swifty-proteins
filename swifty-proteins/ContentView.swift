e80b8f597a2146a4d3cf5026a38a8ca061a3f391
Stina <sluetzen@student.42.fr>
2022-09-05 17:47:55 +0200

    Initialising shifty-proteins project. Added icons. Added Biometric login + error handling.

--- swifty-proteins/ContentView.swift
+++ swifty-proteins/ContentView.swift
@@ -6,12 +6,116 @@
 //
 
 import SwiftUI
+import LocalAuthentication // Needed for Face ID / Biometric auth
 
 struct ContentView: View {
+	@State private var isUnlocked = false
+	@State private var noBiometrics = false
+	
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		VStack {
+			if isUnlocked {
+				if noBiometrics {
+					Text("Your device is not compatible with this application")
+				}
+				else {
+					Text("Unlocked")
+				}
+			}
+			else {
+				Text("Locked")
+				Button("Authenticate") {
+					authenticate()
+				}
+			}
+		}
     }
+	
+	func authenticate() {
+
+		let context = LAContext() 		// Provides info about authentication mechanism on user’s device
+		var error: NSError? // Obj-C
+		
+		// Check whether biometric authentication is possible
+		if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+			// it's possible, so go ahead and use it
+			let reason = "We need you to login in order to access to the application"
+			
+			context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+				// authentication has now completed
+				if success {
+					isUnlocked = true
+					print("auth successful")
+					// authenticated successfully
+				} else {
+					let code = LAError.Code(rawValue: error!.code) // TODO : Verify
+					
+					switch code! {
+					case .appCancel:
+						break;
+						// The app canceled authentication by
+						// invalidating the LAContext
+					case .authenticationFailed:
+						// The user did not provide
+						// valid credentials
+						break;
+					case .invalidContext:
+						// The LAContext was invalid
+						break;
+					case .notInteractive:
+						// Interaction was not allowed so the
+						// authentication failed
+						break;
+					case .passcodeNotSet:
+						// The user has not set a passcode
+						// on this device
+						break;
+					case .systemCancel:
+						// The system canceled authentication,
+						// for example to show another app
+						break;
+					case .userCancel:
+						// The user canceled the
+						// authentication dialog
+						break;
+					case .userFallback:
+						// The user selected to use a fallback
+						// authentication method
+						break;
+					case .biometryLockout:
+						// Too many failed attempts locked
+						// biometric authentication
+						break;
+					case .biometryNotAvailable:
+						// The user's device does not support
+						// biometric authentication
+						break;
+					case .biometryNotEnrolled:
+						// The user has not configured
+						// biometric authentication
+						break;
+					case .touchIDNotAvailable:
+						break;
+					case .touchIDNotEnrolled:
+						break;
+					case .touchIDLockout:
+						break;
+					@unknown default:
+						// An other error occurred
+						break;
+					}
+					print("auth unsuccessful")
+					
+					// there was a problem
+				}
+			}
+			noBiometrics = false
+		} else {
+			print("no biometric auth possible")
+			noBiometrics = true
+			// no biometrics
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
