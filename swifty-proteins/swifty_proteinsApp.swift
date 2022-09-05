d5d12a992b13e23fb7416a61f2d54291eb9278be
Stina <sluetzen@student.42.fr>
2022-09-05 14:32:09 +0200

    Initial Commit

---
+++ swifty-proteins/swifty_proteinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifty_proteinsApp.swift
+//  swifty-proteins
+//
+//  Created by Stina on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct swifty_proteinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
